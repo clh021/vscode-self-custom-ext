22f2dfe890d84e4394625d5723a5e0f88ae80901
chenlianghong <clh021@gmail.com>
2022-08-20 11:29:41 +0800

    add comment for service

--- src/service/healthNotify/index.ts
+++ src/service/healthNotify/index.ts
@@ -1,7 +1,12 @@
 // 身体健康提醒
+// 定时提醒:
+// 活动颈椎
+// 保护眼睛
+// 站立起身
+// 走两步
 import * as vscode from 'vscode';
 const healthNotify = vscode.commands.registerCommand('lianghongCustom.laozi', () => {
     vscode.window.showInformationMessage('laozi from 良宏自用插件!');
 });
 
-export default healthNotify
+export default healthNotify;
--- src/service/teaNotify/index.ts
+++ src/service/teaNotify/index.ts
@@ -1,7 +1,8 @@
 // 喝水提醒
+// 根据编辑器持续的活动时间达到一定临界值提醒
 import * as vscode from 'vscode';
 const teaNotify = vscode.commands.registerCommand('lianghongCustom.laozi', () => {
     vscode.window.showInformationMessage('laozi from 良宏自用插件!');
 });
 
-export default teaNotify
+export default teaNotify;
